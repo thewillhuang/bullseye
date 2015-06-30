e8e2daa426e9634374fd8a144c1aa0479f0b9655
William Huang <will.h86@gmail.com>
2015-06-29 22:36:06 -0700

    Initial Commit

---
+++ bullseye/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bullseye
+//
+//  Created by Wei liang huang on 6/29/15.
+//  Copyright (c) 2015 Wei liang huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
