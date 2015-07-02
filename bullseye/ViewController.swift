4a3b023fb9ae27e2305e094ac9431bdf29e89b0b
William Huang <will.h86@gmail.com>
2015-07-02 14:52:20 -0700

    swift tutorial

--- bullseye/ViewController.swift
+++ bullseye/ViewController.swift
@@ -9,17 +9,49 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var slider: UISlider!
+    
+    var currentValue: Int = 0
+    var targetValue: Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        currentValue = lroundf(slider.value)
+        targetValue = 1 + Int(arc4random_uniform(100))
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func sliderMoved(slider: UISlider) {
+        println("The value of the slider is now :\(slider.value)")
+        currentValue = lroundf(slider.value)
+    }
+    
+    @IBAction func showAlert() {
+        let message = "the current value of the slider is: \(currentValue)"
+                    + "\nThe target value is: \(targetValue)"
+        
+        let alert = UIAlertController(
+            title: "Hello, World",
+            message: message,
+            preferredStyle: .Alert
+        )
+        
+        let action = UIAlertAction(
+            title: "Ok",
+            style: .Default,
+            handler: nil
+        )
+        
+        alert.addAction(action)
+        
+        presentViewController(alert, animated: true, completion: nil)
+    }
 
 }
 
